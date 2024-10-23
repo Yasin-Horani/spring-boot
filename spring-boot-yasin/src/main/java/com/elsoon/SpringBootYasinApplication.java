5ae2709b7047424af53a0fe5849cdf716d8e4866
cout  "Yasin Horani <70699122+yasin-horani@users.noreply.github.com>
2024-10-23 14:00:21 +0200

    Initial the spring boot project

---
+++ spring-boot-yasin/src/main/java/com/elsoon/SpringBootYasinApplication.java
@@ -0,0 +1,13 @@
+package com.elsoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootYasinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootYasinApplication.class, args);
+	}
+
+}
